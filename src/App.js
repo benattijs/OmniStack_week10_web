2ee35d31fddf83202bd942d54010b7904ffb346f
Jonatah <benattijs@gmail.com>
2020-01-18 09:26:17 +1300

    End of video course

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './global.css'
+import './App.css'
+import './SideBar.css'
+import './Main.css'
+import './services/api'
+import api from './services/api';
+
+
+import './components/DevItem/index'
+import DevForm from './components/DevForm/';
+import DevItem from './components/DevItem/';
 
 function App() {
+
+  const [devList, setDevList] = useState([{"test":"OK"}]);
+
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');     
+      setDevList(response.data);
+    }
+
+    loadDevs();
+
+  }, []);
+
+
+  async function handleAddDev(data) {
+    
+    const response = await api.post('/devs', data)
+    
+    setDevList([...devList, response.data]);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Register</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devList.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
